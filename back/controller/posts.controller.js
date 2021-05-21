64a0effd4cbbeacd06ba8f3029eedab33e611167
Matias <matiasalarcon55@hotmail.com>
2021-05-21 15:54:08 -0300

    Uso de axios para peticionar a json placeholder

--- back/controller/posts.controller.js
+++ back/controller/posts.controller.js
@@ -1,5 +1,9 @@
-const getPosts = (req, res) => {
-    res.json({ posts: 'posts' });
+const axios = require('axios');
+
+const getPosts = async (req, res) => {
+    const request = await axios.get('https://jsonplaceholder.typicode.com/posts');
+    const posts = request.data;
+    res.json({ posts });
 };
 
 module.exports = { getPosts };
