ec291e3687c2ed138aa0cd9ccbd9b320e2bd28dc
Matias <matiasalarcon55@hotmail.com>
2021-05-21 16:48:55 -0300

    Implementacion de bulma

--- front/pages/posts.js
+++ front/pages/posts.js
@@ -1,5 +1,16 @@
+import Head from 'next/head';
+import 'bulma/css/bulma.min.css';
+
 const Posts = () => {
-    return <h1>Posts</h1>;
+    return (
+        <>
+            <Head>
+                <meta name='viewport' content='width=device-width, initial-scale=1' />
+                <title>Posts</title>
+            </Head>
+            <h1>Posts</h1>
+        </>
+    );
 };
 
 export default Posts;
