1adc3712163438839fd3b2269874a5cfa286b9b0
Matias <matiasalarcon55@hotmail.com>
2021-05-21 20:24:56 -0300

    Config inicial de redux

--- front/components/Layout.js
+++ front/components/Layout.js
@@ -1,16 +1,21 @@
 import Head from 'next/head';
+import { Provider } from 'react-redux';
+import store from '../app/store';
 import Navbar from './Navbar';
 import 'bulma/css/bulma.min.css';
 
 const Layout = (props) => {
     return (
         <>
-            <Head>
-                <meta name='viewport' content='width=device-width, initial-scale=1' />
-                <title>{props.title} - Front</title>
-            </Head>
-            <Navbar />
-            <div className='container mt-3'>{props.children}</div>
+            <Provider store={store}>
+                <Head>
+                    <meta name='viewport' content='width=device-width, initial-scale=1' />
+                    <title>{props.title} - Front</title>
+                </Head>
+
+                <Navbar />
+                <div className='container mt-3'>{props.children}</div>
+            </Provider>
         </>
     );
 };
