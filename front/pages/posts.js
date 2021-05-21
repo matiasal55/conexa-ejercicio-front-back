3e661e3015a2395071104149b2c271f26fa293a6
Matias <matiasalarcon55@hotmail.com>
2021-05-21 17:00:02 -0300

    Uso de getInitialProps

--- front/pages/posts.js
+++ front/pages/posts.js
@@ -1,16 +1,22 @@
 import Head from 'next/head';
+import { getRequest } from '../utils/requestHandler';
 import 'bulma/css/bulma.min.css';
 
-const Posts = () => {
+const Posts = (props) => {
     return (
         <>
             <Head>
                 <meta name='viewport' content='width=device-width, initial-scale=1' />
                 <title>Posts</title>
             </Head>
-            <h1>Posts</h1>
+            <h1 className='container'>Posts</h1>
         </>
     );
+};
+
+Posts.getInitialProps = async (ctx) => {
+    const res = await getRequest('http://localhost:4000/posts');
+    return { posts: res.data };
 };
 
 export default Posts;
