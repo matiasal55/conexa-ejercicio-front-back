b8a6d32ee43fb279b5bb5451ebe5e2bb64de2226
Matias <matiasalarcon55@hotmail.com>
2021-05-21 15:30:41 -0300

    Config express

--- back/app.js
+++ back/app.js
@@ -1,4 +1,13 @@
 const express = require('express');
+const logger = require('morgan');
 const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use((req, res) => {
+    res.status(400).json({ message: 'Bad request' });
+});
 
 module.exports = app;
--- back/index.js
+++ back/index.js
@@ -1,7 +1,7 @@
 const app = require('./app');
 require('dotenv').config();
 
-const port = process.env.SERVER_PORT || 3000;
+const port = process.env.SERVER_PORT || 4000;
 app.listen(port, () => {
     console.log('Connected to server to port ' + port);
 });
