79cad853805b401afc19bea717013eb4d073e73b
Matias <matiasalarcon55@hotmail.com>
2021-05-22 00:11:03 -0300

    Implementacion de CORS en back

--- back/app.js
+++ back/app.js
@@ -3,12 +3,14 @@ const logger = require('morgan');
 
 const postsRouter = require('./routes/posts.routes');
 const photosRouter = require('./routes/photos.routes');
+const cors = require('cors');
 
 const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 
 app.use('/posts', postsRouter);
 app.use('/photos', photosRouter);
