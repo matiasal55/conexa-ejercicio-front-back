cc17320ef28f8f4589891394e0793cd07bbba8c2
Matias <matiasalarcon55@hotmail.com>
2021-05-21 23:56:30 -0300

    Config de reducer de posts

--- front/app/store.js
+++ front/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import photosReducer from '../features/photosSlice';
+import postsReducer from '../features/postsSlice';
 
 export default configureStore({
     reducer: {
         photos: photosReducer,
+        posts: postsReducer,
     },
 });
---
+++ front/features/postsSlice.js
@@ -0,0 +1,29 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { getRequest } from '../utils/requestHandler';
+
+const postsSlice = createSlice({
+    name: 'posts',
+    initialState: {
+        postsList: [],
+    },
+    reducers: {
+        setPostsList: (state, action) => {
+            state.postsList = action.payload;
+        },
+    },
+});
+
+export const { setPostsList } = postsSlice.actions;
+
+export const getPosts = () => async (dispatch) => {
+    try {
+        const request = await getRequest('http://localhost:4000/photos');
+        dispatch(setPostsList(request.posts));
+    } catch (e) {
+        dispatch(setPostsList([]));
+    }
+};
+
+export const postsList = (state) => state.posts.postsList;
+
+export default postsSlice.reducer;
