67354655ed0a22a04c10aeb3353079b721f2f1a3
Matias <matiasalarcon55@hotmail.com>
2021-05-22 19:32:06 -0300

    Implementacion de try catch y status

--- back/controller/photos.controller.js
+++ back/controller/photos.controller.js
@@ -1,12 +1,16 @@
 const { getRequest } = require('../services/requestHandler');
 
 const getPhotos = async (req, res) => {
-    const page = req.params.page;
-    const limit = 10;
-    const request = await getRequest(`https://jsonplaceholder.typicode.com/photos?_page=${page}&_limit=${limit}`);
-    const maxSize = request.headers['x-total-count'];
-    const photos = request.data;
-    res.json({ photos, maxSize });
+    try {
+        const page = req.params.page;
+        const limit = 10;
+        const request = await getRequest(`https://jsonplaceholder.typicode.com/photos?_page=${page}&_limit=${limit}`);
+        const maxSize = request.headers['x-total-count'];
+        const photos = request.data;
+        res.status(200).json({ photos, maxSize });
+    } catch (e) {
+        res.status(500).json({ message: 'Internal error' });
+    }
 };
 
 module.exports = { getPhotos };
--- back/controller/posts.controller.js
+++ back/controller/posts.controller.js
@@ -1,12 +1,16 @@
 const { getRequest } = require('../services/requestHandler');
 
 const getPosts = async (req, res) => {
-    const page = req.params.page;
-    const limit = 10;
-    const request = await getRequest(`https://jsonplaceholder.typicode.com/posts?_page=${page}&_limit=${limit}`);
-    const maxSize = request.headers['x-total-count'];
-    const posts = request.data;
-    res.json({ posts, maxSize });
+    try {
+        const page = req.params.page;
+        const limit = 10;
+        const request = await getRequest(`https://jsonplaceholder.typicode.com/posts?_page=${page}&_limit=${limit}`);
+        const maxSize = request.headers['x-total-count'];
+        const posts = request.data;
+        res.status(200).json({ posts, maxSize });
+    } catch (e) {
+        res.status(500).json({ message: 'Internal error' });
+    }
 };
 
 module.exports = { getPosts };
