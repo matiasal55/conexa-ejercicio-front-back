1d3507feadc86f7550ae8cd19a9230407c9dc036
Matias <matiasalarcon55@hotmail.com>
2021-05-22 11:48:40 -0300

    El request ahora trae la cantidad total de registros

--- back/controller/photos.controller.js
+++ back/controller/photos.controller.js
@@ -4,8 +4,9 @@ const getPhotos = async (req, res) => {
     const page = req.params.page;
     const limit = 10;
     const request = await getRequest(`https://jsonplaceholder.typicode.com/photos?_page=${page}&_limit=${limit}`);
+    const maxSize = request.headers['x-total-count'];
     const photos = request.data;
-    res.json({ photos });
+    res.json({ photos, maxSize });
 };
 
 module.exports = { getPhotos };
--- back/controller/posts.controller.js
+++ back/controller/posts.controller.js
@@ -4,8 +4,9 @@ const getPosts = async (req, res) => {
     const page = req.params.page;
     const limit = 10;
     const request = await getRequest(`https://jsonplaceholder.typicode.com/posts?_page=${page}&_limit=${limit}`);
+    const maxSize = request.headers['x-total-count'];
     const posts = request.data;
-    res.json({ posts });
+    res.json({ posts, maxSize });
 };
 
 module.exports = { getPosts };
