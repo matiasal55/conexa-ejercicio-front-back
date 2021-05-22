cbe1735dffbcecb85ac4793d8d479abde4fc590b
Matias <matiasalarcon55@hotmail.com>
2021-05-22 15:50:29 -0300

    Creacion inicial de index

---
+++ front/pages/index.js
@@ -0,0 +1,16 @@
+import Layout from '../components/Layout';
+
+const Index = () => {
+    return (
+        <Layout title='Home'>
+            <div className='has-text-centered'>
+                <div>
+                    <img src='https://conexa.ai/wp-content/uploads/2021/03/logo.svg' alt='logo-conexa' />
+                </div>
+                <h1 className='is-size-5'>Ejercicio Front / Back</h1>
+            </div>
+        </Layout>
+    );
+};
+
+export default Index;
