f9b453c2d4f828acc2061178c29ac1ca3f4cfe3b
Matias <matiasalarcon55@hotmail.com>
2021-05-22 23:48:18 -0300

    Se ocultan las secciones de la pagina cuando no esta logueado

--- front/components/Navbar.js
+++ front/components/Navbar.js
@@ -1,8 +1,12 @@
 import Link from 'next/link';
 import { useState } from 'react';
+import { useSelector } from 'react-redux';
+import { existsToken } from '../features/userSlice';
 
 const Navbar = () => {
     const [isActive, setIsActive] = useState(false);
+    const tokenState = useSelector(existsToken);
+
     return (
         <nav className='navbar is-warning' role='navigation' aria-label='main navigation'>
             <div className='navbar-brand'>
@@ -11,38 +15,42 @@ const Navbar = () => {
                         <img src='https://conexa.ai/wp-content/uploads/2021/03/logo.svg' width={112} height={28} />
                     </a>
                 </Link>
-                <a
-                    role='button'
-                    onClick={() => setIsActive(!isActive)}
-                    className={`navbar-burger burger${isActive ? ' is-active' : ''}`}
-                    aria-label='menu'
-                    aria-expanded='false'
-                    data-target='navbarBasicExample'
-                >
-                    <span aria-hidden='true' />
-                    <span aria-hidden='true' />
-                    <span aria-hidden='true' />
-                </a>
+                {tokenState ? (
+                    <a
+                        role='button'
+                        onClick={() => setIsActive(!isActive)}
+                        className={`navbar-burger burger${isActive ? ' is-active' : ''}`}
+                        aria-label='menu'
+                        aria-expanded='false'
+                        data-target='navbarBasicExample'
+                    >
+                        <span aria-hidden='true' />
+                        <span aria-hidden='true' />
+                        <span aria-hidden='true' />
+                    </a>
+                ) : null}
             </div>
-            <div id='navbarBasicExample' className={`navbar-menu${isActive ? ' is-active' : ''}`}>
-                <div className='navbar-start'>
-                    <Link href='/posts'>
-                        <a className='navbar-item'>Posts</a>
-                    </Link>
-                    <Link href='/photos'>
-                        <a className='navbar-item'>Photos</a>
-                    </Link>
-                </div>
-                <div className='navbar-end'>
-                    <div className='navbar-item'>
-                        <div className='buttons'>
-                            <a className='button is-info'>
-                                <strong>Logout</strong>
-                            </a>
+            {tokenState ? (
+                <div id='navbarBasicExample' className={`navbar-menu${isActive ? ' is-active' : ''}`}>
+                    <div className='navbar-start'>
+                        <Link href='/posts'>
+                            <a className='navbar-item'>Posts</a>
+                        </Link>
+                        <Link href='/photos'>
+                            <a className='navbar-item'>Photos</a>
+                        </Link>
+                    </div>
+                    <div className='navbar-end'>
+                        <div className='navbar-item'>
+                            <div className='buttons'>
+                                <a className='button is-info'>
+                                    <strong>Logout</strong>
+                                </a>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
+            ) : null}
         </nav>
     );
 };
