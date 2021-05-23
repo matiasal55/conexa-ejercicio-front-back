d9a024a5726684529ca978e27dfa34107ce1474a
Matias <matiasalarcon55@hotmail.com>
2021-05-22 21:47:15 -0300

    Se agrego el metodo post

--- front/utils/requestHandler.js
+++ front/utils/requestHandler.js
@@ -4,3 +4,8 @@ export const getRequest = async (route) => {
     const request = await axios.get(route);
     return request.data;
 };
+
+export const postRequest = async (route, data) => {
+    const request = await axios.post(route, data);
+    return request.data;
+};
