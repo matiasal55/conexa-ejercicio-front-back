a447e09c9e823b48278b21cace553c8aea37182a
Matias <matiasalarcon55@hotmail.com>
2021-06-06 19:49:51 -0300

    Puerto removido

--- back/index.js
+++ back/index.js
@@ -2,7 +2,7 @@ const app = require('./app');
 require('dotenv').config();
 require('./config/db');
 
-const port = process.env.SERVER_PORT || 4000;
+const port = process.env.SERVER_PORT;
 app.listen(port, () => {
     console.log('Connected to server to port ' + port);
 });
