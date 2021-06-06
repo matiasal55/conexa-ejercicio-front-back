6b8d057f92e1d33232e80878a94dbeb0609726c0
Matias <matiasalarcon55@hotmail.com>
2021-06-06 12:04:26 -0300

    Proceso de verificacion de datos ingresados apartados en una funcion

--- back/controller/users.controller.js
+++ back/controller/users.controller.js
@@ -3,33 +3,37 @@ const { login, register, dataUser } = require('../services/users.services');
 const { success, error, defineError } = require('../messages/resMessages');
 const { validateToken } = require('../services/validateToken');
 
+const verifyValidation = (req) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        throw new Error('Error de llenado de campos');
+    }
+};
+
 const loginUser = async (req, res) => {
     try {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return error(res, errors);
-        }
+        const errors = verifyValidation(req);
         const data = req.body;
         const user = await login(data);
         if (!user) return error(res, 'Incorrect user or password');
         return success(res, user);
     } catch (e) {
-        return error(res, 'Internal Error', 500);
+        return defineError(res, e.message, 'campos', 'Campos ingresados incorrectos', 400);
     }
 };
 
 const registerUser = async (req, res) => {
     try {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return error(res, errors);
-        }
+        const errors = verifyValidation(req);
         const data = req.body;
         if (data.password !== data.repassword) return error(res, 'Las contraseñas no coinciden');
         const user = await register(data);
         return success(res, user, 201);
     } catch (e) {
-        defineError(res, e.message, 'E11000', 'User data already exists', 400);
+        if (e.message && e.message.includes('campos')) {
+            return error(res, 'Campos ingresados incorrectos');
+        }
+        return defineError(res, e.message, 'E11000', 'User data already exists', 400);
     }
 };
 
